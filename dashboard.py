104f78d6972febf5c22f0a117ff4a83a6fdc8c77
hp5099499 <122427384+hp5099499@users.noreply.github.com>
2024-08-29 10:09:37 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -165,9 +165,6 @@ def display_indices():
 # Main function to run the app
 def main():
     st.title("Stock Market Dashboard")
-    
-    with open("style.css") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
     st.markdown("<div class='custom-header'>Top Gainers</div>", unsafe_allow_html=True)
     fetch_gainers()
