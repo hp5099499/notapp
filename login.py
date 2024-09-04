752d5ada5f2c9bf0c20552791bdc2c3fea65c43b
Mohit Maurya <74113320+mohitmaurya2023@users.noreply.github.com>
2024-09-04 16:33:34 +0530

    Update login.py

--- login.py
+++ login.py
@@ -548,7 +548,7 @@ def logout():
     st.session_state["logged_in"] = False
     st.session_state["username"] = ""
     st.session_state["email"] = ""
-    st.rerun()
+    # st.rerun()
 
 # Main Function with Navigation
 
